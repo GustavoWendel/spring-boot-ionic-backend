5546ee832bd1423c7db1b0fc5d127a755ae863ad
GustavoWendel <gustavo_wendel@hotmail.com>
2021-06-15 22:56:32 -0300

    Clientes, TipoCliente, enderecos e telefones

--- src/main/java/com/gustavowendel/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavowendel/cursomc/CursomcApplication.java
@@ -1,20 +1,26 @@
 package com.gustavowendel.cursomc;
 
-import com.gustavowendel.cursomc.domain.Categoria;
-import com.gustavowendel.cursomc.domain.Cidade;
-import com.gustavowendel.cursomc.domain.Estado;
-import com.gustavowendel.cursomc.domain.Produto;
-import com.gustavowendel.cursomc.repositories.CategoriaRepository;
-import com.gustavowendel.cursomc.repositories.CidadeRepository;
-import com.gustavowendel.cursomc.repositories.EstadoRepository;
-import com.gustavowendel.cursomc.repositories.ProdutoRepository;
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Collections;
+import com.gustavowendel.cursomc.domain.Categoria;
+import com.gustavowendel.cursomc.domain.Cidade;
+import com.gustavowendel.cursomc.domain.Cliente;
+import com.gustavowendel.cursomc.domain.Endereco;
+import com.gustavowendel.cursomc.domain.Estado;
+import com.gustavowendel.cursomc.domain.Produto;
+import com.gustavowendel.cursomc.domain.enums.TipoCliente;
+import com.gustavowendel.cursomc.repositories.CategoriaRepository;
+import com.gustavowendel.cursomc.repositories.CidadeRepository;
+import com.gustavowendel.cursomc.repositories.ClienteRepository;
+import com.gustavowendel.cursomc.repositories.EnderecoRepository;
+import com.gustavowendel.cursomc.repositories.EstadoRepository;
+import com.gustavowendel.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -30,6 +36,12 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -64,7 +76,19 @@ public class CursomcApplication implements CommandLineRunner{
 		est1.getCidades().addAll(Collections.singletonList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
---
+++ src/main/java/com/gustavowendel/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.gustavowendel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustavowendel.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
